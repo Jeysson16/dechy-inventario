e235d62c1b224f36f0609dcdb1500b0342cfab90
Jeysson Sánchez Rodríguez <jeysson_s.r@hotmail.com>
2026-03-04 19:21:17 -0500

    Revert storage bucket domain to fix CORS origin and add cors.json

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDzPYYgwvGcYng9ddI4A8nXEpLasoMxXf4",
   authDomain: "inventory-app-jey-123.firebaseapp.com",
   projectId: "inventory-app-jey-123",
-  storageBucket: "inventory-app-jey-123.appspot.com",
+  storageBucket: "inventory-app-jey-123.firebasestorage.app",
   messagingSenderId: "225468681713",
   appId: "1:225468681713:web:af0b4bb8c73a3237520850"
 };
